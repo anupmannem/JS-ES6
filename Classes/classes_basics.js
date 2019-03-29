9c964812d28bf123a2731c7908c7c24781806eb5
anupmannem <anupmannem@gmail.com>
2019-03-29 15:22:10 +0530

    constructor and super

--- Classes/classes_basics.js
+++ Classes/classes_basics.js
@@ -115,3 +115,43 @@ console.log(cx5.carStats());
 console.log(civic);
 console.log(civic.carStats());
 console.log(Car.totalDoors(cx5, cx6));
+
+// constructor and super
+class Car {
+    constructor(doors, engine, color) {
+        this.doors = doors;
+        this.engine = engine;
+        this.color = color;
+    }
+
+    carStats() {
+        return `this car has ${this.doors} doors, a ${
+            this.engine
+        } engine and a beautiful ${this.color} color!`;
+    }
+
+    static totalDoors(car1, car2) {
+        const doors1 = car1.doors;
+        const doors2 = car2.doors;
+        return doors1 + doors2;
+    }
+}
+
+class SUV extends Car {
+    constructor(doors, engine, color, brand, carStats) {
+        // to be able to use doors, engine, color, carStats from Car
+        super(doors, engine, color, carStats); // create another object
+        this.brand = brand;
+        this.wheels = 4;
+        this.ac = true;
+    }
+
+    myBrand() {
+        return console.log(`this SUV is a ${this.brand}`);
+    }
+}
+
+const cx5 = new SUV(4, "V6", "grey", "mazda");
+console.log(cx5);
+console.log(cx5.carStats());
+console.log(cx5.myBrand());

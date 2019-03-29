a73a8837f5ade848f5114901bd782001137b7807
anupmannem <anupmannem@gmail.com>
2019-03-29 15:38:44 +0530

    mixin usage

--- Classes/classes_basics.js
+++ Classes/classes_basics.js
@@ -117,6 +117,19 @@ console.log(civic.carStats());
 console.log(Car.totalDoors(cx5, cx6));
 
 // constructor and super
+// mixin
+let mixin = {
+    madeIn() {
+        console.log("this car was made in year 2019");
+    }
+};
+
+let carMixin = {
+    __proto__: mixin,
+    madeIn() {
+        super.madeIn();
+    }
+};
 class Car {
     constructor(doors, engine, color) {
         this.doors = doors;
@@ -144,6 +157,9 @@ class SUV extends Car {
         this.brand = brand;
         this.wheels = 4;
         this.ac = true;
+
+        // assign mixin
+        Object.assign(this, carMixin);
     }
 
     myBrand() {
@@ -155,3 +171,4 @@ const cx5 = new SUV(4, "V6", "grey", "mazda");
 console.log(cx5);
 console.log(cx5.carStats());
 console.log(cx5.myBrand());
+console.log(cx5.madeIn());

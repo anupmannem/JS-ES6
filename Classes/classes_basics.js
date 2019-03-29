12d844bf62d4e585568ebb3d35207855459e157b
anupmannem <anupmannem@gmail.com>
2019-03-29 14:45:14 +0530

    class components vs functional component

--- Classes/classes_basics.js
+++ Classes/classes_basics.js
@@ -2,26 +2,45 @@
 
 // class declaration
 class Car {
-	constructor(doors, engine, color) {
-		this.doors = doors;
-		this.engine = engine;
-		this.color = color;
-	}
+    constructor(doors, engine, color) {
+        this.doors = doors;
+        this.engine = engine;
+        this.color = color;
+    }
 
-	carStats() {
-		return `this car has ${this.doors} doors, a ${this.engine} engine and a beautiful ${this.color}`;
-	}
+    carStats() {
+        return `this car has ${this.doors} doors, a ${
+            this.engine
+        } engine and a beautiful ${this.color}`;
+    }
 }
 
-const cx5 = new Car(4, 'V6', 'grey');
+const cx5 = new Car(4, "V6", "grey");
 
 // class expression
 const Car = class {
-	constructor(doors, engine, color) {
-		this.doors = doors;
-		this.engine = engine;
-		this.color = color;
-	}
+    constructor(doors, engine, color) {
+        this.doors = doors;
+        this.engine = engine;
+        this.color = color;
+    }
+};
+
+const cx5 = new Car(4, "V6", "grey");
+
+// React class component (stateful)
+class Cloak extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, World!</h1>
+                <h2>It is {this.props.data.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
 }
 
-const cx5 = new Car(4, 'V6', 'grey');
+// React functional component (stateless)
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+}

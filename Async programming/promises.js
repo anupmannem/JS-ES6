8fce14c02c3739f1d09f590d16ca9ab99c628db8
anupmannem <anupmannem@gmail.com>
2019-04-28 12:30:54 +0530

    promises exercises

--- Async programming/promises.js
+++ Async programming/promises.js
@@ -30,3 +30,31 @@ doAsyncTask().then(
     (err) => console.log(err)
 );
 
+/******************************************************************/
+
+const fs = require("fs");
+
+function readFile(filename, encoding) {
+	fs.readFile(filename, encoding, (err, data) => {
+		
+	});	
+}
+readFile("./files/demofile.txt", "utf-8").then();                                              
+
+// promise version of the above
+function readFile(filename, encoding) {
+	return new Promise((resolve, reject) => {
+		fs.readFile(filename, encoding, (err, data) => {
+			if(err) return reject(err);
+			resolve(data);			
+		});	
+	})	
+}
+readFile("./files/demofile.txt", "utf-8").then(
+    val => console.log(val),
+    err => consol.error(err)
+);
+
+// util.promisify() another option to convert into promise verision
+const util = require("util");
+const readFile = util.promisify(fs.readFile);
